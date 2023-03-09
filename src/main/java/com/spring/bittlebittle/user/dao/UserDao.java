9d1a2157f8929bc4461a039957fb056420098841
dlalsgud1815 <katemike@naver.com>
2023-03-09 09:14:12 +0900

    :Feat test

--- src/main/java/com/spring/bittlebittle/user/dao/UserDao.java
+++ src/main/java/com/spring/bittlebittle/user/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.spring.bittlebittle.user.dao;
 
+import com.spring.bittlebittle.user.vo.User;
 import com.spring.bittlebittle.utils.DaoInterface;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.logging.log4j.LogManager;
@@ -15,29 +16,55 @@ public class UserDao implements DaoInterface {
     private Logger log = LogManager.getLogger("case3");
     @Autowired
     private SqlSession sqlSession;
+    
+	@Override
+	public User getUserById(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public User getUserByPwd(int userPwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public List<Object> selectList(Object obj) {
-        return null;
-    }
-
-    @Override
-    public Object selectOne(Object obj) {
-        return null;
-    }
-
-    @Override
-    public int insert(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public int update(Object obj) {
-        return 0;
-    }
-
-    @Override
-    public int delete(Object obj) {
-        return 0;
-    }
+//    @Override
+//    public List<Object> selectList(Object obj) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Object selectOne(Object obj) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int insert(Object obj) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public int update(Object obj) {
+//        return 0;
+//    }
+//
+//    @Override
+//    public int delete(Object obj) {
+//        return 0;
+//    }
 }
--- src/main/java/com/spring/bittlebittle/utils/DaoInterface.java
+++ src/main/java/com/spring/bittlebittle/utils/DaoInterface.java
@@ -2,21 +2,35 @@ package com.spring.bittlebittle.utils;
 
 import java.util.List;
 
+import com.spring.bittlebittle.user.vo.User;
+
 public interface DaoInterface {
 
-    // selectList
-    List<Object> selectList(Object obj);
-
-    // selectOne
-    Object selectOne(Object obj);
-
-    // insert
-    int insert(Object obj);
-
-    // update
-    int update(Object obj);
-
-    // delete
-    int delete(Object obj);
-
+//    // selectList
+//    List<Object> selectList(Object obj);
+//
+//    // selectOne
+//    Object selectOne(Object obj);
+//
+//    // insert
+//    int insert(Object obj);
+//
+//    // update
+//    int update(Object obj);
+//
+//    // delete
+//    int delete(Object obj);
+	
+	
+	User getUserById(String userId);
+	
+	User getUserByPwd(int userPwd);
+	
+	void addUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUser(User user);
+	
+	
 }
