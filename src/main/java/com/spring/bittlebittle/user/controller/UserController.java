548aa02e831d653eeb764ede9f707f1b2b514c22
onehuts <onehuts@naver.com>
2023-03-09 09:24:21 +0900

    테스트

--- src/main/java/com/spring/bittlebittle/user/controller/UserController.java
+++ src/main/java/com/spring/bittlebittle/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.spring.bittlebittle.user.controller;
 
-import com.spring.bittlebittle.user.service.UserService;
-import com.spring.bittlebittle.user.vo.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.bittlebittle.user.service.UserService;
+import com.spring.bittlebittle.user.vo.User;
 
 @Controller
 public class UserController {
