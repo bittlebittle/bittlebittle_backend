75a9e3b51bf3f9aed00309faece6ab61a6fa7218
sjh <zwielicht2@gmail.com>
2023-03-10 18:32:44 +0900

    :Fix commit

--- src/main/java/com/spring/bittlebittle/user/service/UserServiceImpl.java
+++ src/main/java/com/spring/bittlebittle/user/service/UserServiceImpl.java
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> selectList() {
-        return dao.selectList();
+        return dao.selectUsers();
     }
 
     @Override
