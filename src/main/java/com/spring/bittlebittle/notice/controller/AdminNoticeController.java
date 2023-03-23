a6fd2ffc37b5b0e6bf4be4d470814d3415325cd6
onehuts <onehuts@naver.com>
2023-03-23 12:07:53 +0900

    0323 notice

--- src/main/java/com/spring/bittlebittle/notice/controller/AdminNoticeController.java
+++ src/main/java/com/spring/bittlebittle/notice/controller/AdminNoticeController.java
@@ -1,22 +1,34 @@
 package com.spring.bittlebittle.notice.controller;
 
-import com.spring.bittlebittle.notice.service.AdminNoticeServiceImpl;
-import com.spring.bittlebittle.notice.vo.Notice;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.spring.bittlebittle.notice.service.AdminNoticeServiceImpl;
+import com.spring.bittlebittle.notice.vo.Notice;
 
 @RestController
-@RequestMapping("/admin/faqs")
+@RequestMapping("api/admin/notices")
 public class AdminNoticeController {
 	@Autowired
 	private AdminNoticeServiceImpl adminNoticeServiceImpl;
+	
+	Logger log = LogManager.getLogger("case3");
 
-	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Notice> noticeList() {
 		return adminNoticeServiceImpl.getNoticeList();
 	}
@@ -31,22 +43,26 @@ public class AdminNoticeController {
 		}
 	}
 
-	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Notice> addNotice(@RequestBody Notice notice) {
+		log.debug(notice);
 		adminNoticeServiceImpl.addNotice(notice);
 		return new ResponseEntity<>(notice, HttpStatus.CREATED);
 	}
 
-	@PutMapping(value = "/{noticeNo}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/{noticeNo}/set-data", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Notice> updateNotice(@PathVariable int noticeNo, @RequestBody Notice notice) {
 		notice.setNoticeNo(noticeNo);
-
+		log.debug(notice);
 		adminNoticeServiceImpl.updateNotice(notice);
 		return new ResponseEntity<>(notice, HttpStatus.OK);
 	}
 
-	@DeleteMapping(value = "/{noticeNo}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<HttpStatus> deleteNotice(@PathVariable int noticeNo, @RequestBody int userNo) {
+	@GetMapping(value = "/{noticeNo}/deletion", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HttpStatus> deleteNotice(@PathVariable int noticeNo) {
+		
+		log.debug("삭제 소환");
+		adminNoticeServiceImpl.deleteNotice(noticeNo);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
